28daf181c56361d149aadd04405d5de04e92f821
Gerome Yoo <95834874+ashjean7805@users.noreply.github.com>
2024-07-05 00:32:21 +0900

    공백 제거

--- docker/server_stop.sh
+++ docker/server_stop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ###########################################
 ## Docker 컨테이너 이름
-CONTAINER_NAME = "your_container_name"
+CONTAINER_NAME="your_container_name"
 
 ## 이미지 이름 (필요한 경우 이미지 태그도 포함)
-IMAGE_NAME = "your_image_name:latest"
+IMAGE_NAME="your_image_name:latest"
 
 ## 시도 횟수
-ATTEMPTS = 4
+ATTEMPTS=4
 
 ## 슬립타임
-SLEEP_TIME = 3
+SLEEP_TIME=3
 ###########################################
 
 
@@ -19,8 +19,6 @@ docker stop ${CONTAINER_NAME}
 # 컨테이너가 실행 중인지 확인
 if [ $(docker ps --format '{{.Names}}' | grep -w ${CONTAINER_NAME}) ]; then
     # 종료 확인
-    ATTEMPTS=5
-    SLEEP_TIME=3
     for (( i=1; i<=ATTEMPTS; i++ )); do
         echo "Checking if container ${CONTAINER_NAME} is still running... Attempt $i"
         if [ $(docker ps --format '{{.Names}}' | grep -w ${CONTAINER_NAME}) ]; then
